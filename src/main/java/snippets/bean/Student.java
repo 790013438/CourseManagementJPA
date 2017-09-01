091126f9863c78a23f81b1e1f15e0c2691f37450
790013438 <790013438@qq.com>
2017-09-01 14:50:17 +0800

    疏忽少提交一个
    呈上

--- src/main/java/snippets/bean/Student.java
+++ src/main/java/snippets/bean/Student.java
@@ -2,15 +2,33 @@ package snippets.bean;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
+import java.util.Date;
+import java.util.List;
+import javax.persistence.ManyToMany;
+import static javax.persistence.CascadeType.MERGE;
+import static javax.persistence.CascadeType.REFRESH;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+
+@Entity
 @ManagedBean (name="student")
 @RequestScoped
 public class Student extends Person {
 
     private static final long serialVersionUID = 1L;
 
+    @Temporal(TemporalType.DATE)
+    @Column(name="enrolled_since")
     private Date enrolledsince;
+
+    @ManyToMany(cascade = { MERGE, REFRESH })
+    @JoinTable(name = "Course_Student", joinColumns = @JoinColumn(name = "student_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "course_id", referencedColumnName = "id"))
+    private List<Course> courses;
 
     public Date getEnrolledsince() {
         return enrolledsince;
@@ -18,6 +36,14 @@ public class Student extends Person {
 
     public void setEnrolledsince(Date enrolledsince) {
         this.enrolledsince = enrolledsince;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
     }
 
 }
